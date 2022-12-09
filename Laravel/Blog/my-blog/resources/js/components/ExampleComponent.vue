e6aaf25ba47bf4c3ffa5284ba75dc754b39686ad
Tionge6 <tionge362@gmail.com>
2022-12-09 11:43:07 +0200

    commit

---
+++ Laravel/Blog/my-blog/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,15 @@
+
+<template>
+    <v-container>
+    <v-btn> Click Me</v-btn>
+    </v-container>
+    
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
